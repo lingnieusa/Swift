d446f8beaefc8106b2ea76051a116efb0c13eebe
lingnie <33401071+lingnie@users.noreply.github.com>
2022-02-20 17:50:16 -0500

    Metal-Game-Engine-Tutorial-Episode8.0_Meshes_PlayerObject

--- Game Engine Series/Game Engine/Game Engine/Core/Engine.swift
+++ Game Engine Series/Game Engine/Game Engine/Core/Engine.swift
@@ -18,6 +18,8 @@ class Engine {
         
         RenderPipelineStateLibrary.initialize()
         
+        MeshLibrary.Initialize()
+        
     }
     
 }
--- Game Engine Series/Game Engine/Game Engine/Display/Renderer.swift
+++ Game Engine Series/Game Engine/Game Engine/Display/Renderer.swift
@@ -1,29 +1,28 @@
 import MetalKit
 
-
 class Renderer: NSObject {
     
-    var gameObject: GameObject = GameObject()
+    var player = Player()
     
 }
+
 extension Renderer: MTKViewDelegate{
+    
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
         //When the window is resized
     }
     
     func draw(in view: MTKView) {
-        guard let drawable = view.currentDrawable,
-              let renderPassDescriptor = view.currentRenderPassDescriptor else { return }
+        guard let drawable = view.currentDrawable, let renderPassDescriptor = view.currentRenderPassDescriptor else { return }
         
         let commandBuffer = Engine.CommandQueue.makeCommandBuffer()
         let renderCommandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
         
-        renderCommandEncoder?.setRenderPipelineState(RenderPipelineStateLibrary.PipelineState(.Basic))
-        gameObject.render(renderCommandEncoder: renderCommandEncoder!)
-
+        player.render(renderCommandEncoder: renderCommandEncoder!)
         
         renderCommandEncoder?.endEncoding()
         commandBuffer?.present(drawable)
         commandBuffer?.commit()
     }
+    
 }
